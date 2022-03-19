326dfd988d204c4e0ae756cb861bbfca8087fc79
Oskar Sharipov <oskarsh[at]riseup[dot]net>
2022-03-19 13:06:59 +0300

    Bump version to v1.0.3

--- forest-bot/__init__.py
+++ forest-bot/__init__.py
@@ -7,7 +7,7 @@ from .utils import Env, VersionInfo
 
 __all__ = ("FOREST_CHAT_ID", "bot", "__version__")
 
-__version__ = VersionInfo(1, 0, 2)
+__version__ = VersionInfo(1, 0, 3)
 
 logging.basicConfig(level=logging.INFO)
 
