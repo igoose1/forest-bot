5c1dce783445116701ffac1337abd975d6a10d39
Oskar Sharipov <oskarsh[at]riseup[dot]net>
2022-07-16 00:56:53 +0300

    Upgrade packages in dev-requirements.txt
    I haven't upgraded requirements.txt as pyrogram was released v2.0, there
    is a long "Transition guide" and pyrogram features aren't covered with
    tests.

--- forest-bot/__init__.py
+++ forest-bot/__init__.py
@@ -19,7 +19,7 @@ from .utils import Env, VersionInfo
 
 __all__ = ("FOREST_CHAT_ID", "bot", "__version__")
 
-__version__ = VersionInfo(2, 1, 4)
+__version__ = VersionInfo(2, 2, 0)
 
 logging.basicConfig(
     level=logging.INFO,
